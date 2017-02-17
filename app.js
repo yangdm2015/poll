9c4756f741ae4c59f21c85ee2d3a535b6ea706df
yangdm2015 <yangshan503@126.com>
2017-02-17 11:10:45 +0800

    mongose test
    Signed-off-by: yangdm2015 <yangshan503@126.com>

--- app.js
+++ app.js
@@ -4,17 +4,19 @@ var express = require('express');
 var routes = require('./src/server/routers/routers');
 var http = require('http');
 var path = require('path');
-/*var mongoose = require('mongoose');
-
-var dbUrl = 'mongodb://localhost/remwords'
+var mongoose = require('mongoose');
+var mongoset = require('./src/server/db/mongoset')
+/*var dbUrl = 'mongodb://localhost/remwords'
 mongoose.connect(dbUrl);*/
-
+mongoset.init()
 var app = express();
 
 app.set('port', process.env.VCAP_APP_PORT || 18080);
 app.set('views', path.join(__dirname, './src/client/views'));
 app.use(express.static(path.join(__dirname, 'src/client')));
 app.set('view engine', 'jade');
+
+console.log("process.env.SERVER_SOFTWARE:",process.env.SERVER_SOFTWARE)
 // Handle Errors gracefully
 app.use(function(err, req, res, next) {
   if(!err) return next();
--- src/server/models/words.js
+++ src/server/models/words.js
@@ -1,6 +1,9 @@
 var mongoose = require('mongoose');
+var mongoset = require('../db/mongoset')
 var WordSchema = new mongoose.Schema({
   word:String
 });
-var Words = mongoose.model('Words',WordSchema)
+/*
+var Words = mongoose.model('Words',WordSchema)*/
+var Words = mongoset.db.model('Words',WordSchema)
 module.exports = Words
