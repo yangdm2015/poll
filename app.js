bb947053b69721498c616f03c45f04f63bd2012a
yangdm2015 <yangshan503@126.com>
2017-03-06 16:57:06 +0800

    sockit Signed-off-by: yangdm2015 <yangshan503@126.com>

--- app.js
+++ app.js
@@ -13,8 +13,12 @@ var testRedis = require('./src/server/db/redisset')
 
 var COOKIE_SECRET = 'keyboard cat';
 var COOKIE_NAME = 'sid';
+
 mongoset.init()
 var app = express();
+app.use(bodyParser());
+app.use(cookieParser(COOKIE_SECRET));
+
 
 var rstore=new RedisStore({
     client:testRedis.returnclient()
@@ -22,9 +26,22 @@ var rstore=new RedisStore({
     port: 6379*/
 })
 
+app.use(session({
+    name: COOKIE_NAME,
+    store: rstore,
+    secret: COOKIE_SECRET,
+    saveUninitialized: true,
+    resave: false,
+    cookie: {
+        path: '/',
+        httpOnly: true,
+        secure: false,
+        maxAge: 1000*60*60*24
+    }
+}));
 
-app.use(bodyParser());
-app.use(cookieParser(COOKIE_SECRET));
+
+
 
 app.set('port', process.env.VCAP_APP_PORT || 18080);
 app.set('views', path.join(__dirname, './src/client/views'));
@@ -47,3 +64,5 @@ var server = http.createServer(app);
 server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+var io = require('socket.io')(server);
+io.sockets.on('connection', pollctrl.vote);
