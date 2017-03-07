84dc1d5e5b410a63756ad27cf9d67c81b0afe39d
yangdm2015 <yangshan503@126.com>
2017-03-07 09:20:19 +0800

    pollruters
    Signed-off-by: yangdm2015 <yangshan503@126.com>

---
+++ src/server/routers/pollrouters.js
@@ -0,0 +1,45 @@
+// Connect to MongoDB using Mongoose
+var mongoose = require('mongoose');
+var COOKIE = require('cookie');
+/*var cobj = require('../public/javascripts/cobj')*/
+/*var passport = require('passport');*/
+var pollctrl = require('../controller/pollctrl')
+var userctrl = require('../controller/userctrl')
+
+/*var db;*/
+/*if (process.env.VCAP_SERVICES) {
+   var env = JSON.parse(process.env.VCAP_SERVICES);
+   db = mongoose.createConnection(env['mongodb-2.2'][0].credentials.url);
+} else {
+   db = mongoose.createConnection('localhost', 'pollsapp');
+}*/
+   /*db = mongoose.createConnection('localhost', 'pollsapp');*/
+
+
+
+
+// Get Poll schema and model
+
+
+
+module.exports = function(app){
+
+  /*pollcontroller*/
+  app.get('/',pollctrl.index)
+  app.get('/polls/polls',pollctrl.list)
+  app.get('/polls/:id', pollctrl.poll);
+  app.get('/mypolls/:created_user', pollctrl.mypolls);
+  app.get('/myvotes/:current_user', pollctrl.myvotes);
+  app.post('/polls',pollctrl.create)
+  app.post('/vote',pollctrl.vote)
+
+
+
+  /*usercontroller*/
+  app.post('/user/register',userctrl.register)
+  app.post('/user/login',userctrl.login)
+  app.get('/user/logout',userctrl.logout)
+  app.get('/user/status',userctrl.status)
+  app.get('/user/user',userctrl.getuser)
+}
+
