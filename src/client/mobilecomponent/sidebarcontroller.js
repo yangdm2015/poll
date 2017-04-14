4ef02443c2050d035e37baee4c4c6ec428c22485
yangdm2015 <yangshan503@126.com>
2017-04-14 11:29:34 +0800

    polllist
    Signed-off-by: yangdm2015 <yangshan503@126.com>

--- src/client/mobilecomponent/sidebarcontroller.js
+++ src/client/mobilecomponent/sidebarcontroller.js
@@ -202,3 +202,338 @@ polls1.service('messageService', ['$rootScope', function($rootScope) {
     }
   };
 }]);
+
+
+
+polls1.directive('dragToDismiss', ['$drag', '$parse','$timeout','$transform',function($drag, $parse, $timeout,$transform) {
+  return {
+    restrict: 'A',
+    compile: function(elem, attrs) {
+      var dismissFn = $parse(attrs.dragToDismiss);
+      return function(scope,elem){
+        var dismiss = false;
+        var isfirst = true,initransform;
+        $drag.bind(elem, {
+          transform: function(element, transform, touch) {
+            if(isfirst){initransform=transform;isfirst=false;}
+
+            if(touch.distanceX>=-touch.rect.width / 4){
+              if(touch.distanceX <= 0){
+                transform.translateX = touch.distanceX
+                transform.translateY = 0;
+              }else{
+                $transform.set(elem,initransform)
+              }
+            }
+            return transform;
+          },
+          move: function(drag) {
+            if (drag.distanceX<=-drag.rect.width / 8) {
+              dismiss = true;
+            } else {
+              dismiss = false;
+            }
+          },
+          cancel: function() {
+            elem.removeClass('dismiss');
+          },
+          end: function(drag) {
+            if (!dismiss) {
+              drag.reset();
+            }else{
+              drag.transform.translateX = -drag.rect.width /4;
+              $transform.set(elem,drag.transform)
+            }
+          }
+        });
+      }
+    }
+  };
+}])
+
+polls1.controller('sidebarcontroller', ['userservice','messageService','SharedState','$scope',function(userservice,messageService,SharedState,$scope){
+  messageService.subscribe('userlogin',function(event, data) {
+    $scope.islogin = data.islogin;
+    $scope.account=data.account;
+  })
+  $scope.islogin=userservice.getUserStatussync().islogin;
+  $scope.account=userservice.getUserStatussync().account;
+}])
+
+
+
+polls1.controller('myfavorcontroller',['$scope','SharedState','commentservice','messageService','userservice','socket','$location','$anchorScroll','$timeout',function($scope,SharedState,commentservice,messageService,userservice,socket,$location,$anchorScroll,$timeout){
+
+
+  $scope.togglemyfavor = function(favorindex) {
+    if(SharedState.get('myfavor')==favorindex){
+      SharedState.setOne('myfavor',-1)
+    }else{
+      SharedState.setOne('myfavor',favorindex)
+    }
+  }
+  $scope.deletefavor = function(favor) {
+    var pollid = favor.poll;
+    userservice.deletefrommyfavor(pollid)
+    .then(function(data){
+      messageService.publish('userupdate',{user:data.user})
+      $scope.user=data.user;
+      $scope.favor=false;
+    })
+  };
+  $scope.gopolldetail=function(favor){
+     $location.url(favor.url);
+  }
+  function initscope(){
+    userservice.getUserstatusfromdb()
+    .then(function(result){
+      $scope.user=result.data.user;
+      $scope.islogin = result.data.islogin;
+    })
+  }
+  initscope()
+}])
+
+
+polls1.controller('msgController',['$scope','SharedState','commentservice','messageService','userservice','socket','$location','$anchorScroll','$timeout',function($scope,SharedState,commentservice,messageService,userservice,socket,$location,$anchorScroll,$timeout){
+  messageService.subscribe('userupdate',function(event,data){
+    $scope.user=data.user;
+  })
+  messageService.subscribe('userlogin',function(event, data) {
+    $scope.user = data.user;
+    $scope.islogin = data.islogin;
+    initscope();
+  })
+  socket.on('newmsg',function(data){
+    $scope.msgrem=true;
+    initscope()
+  })
+  function initscope(){
+    userservice.getUserstatusfromdb()
+    .then(function(result){
+      if(result.data.user){
+        $scope.user=result.data.user;
+        $scope.islogin = result.data.islogin;
+      }
+    })
+  }
+
+  initscope();
+  $scope.tt=function(){
+    console.log('sadfge')
+  }
+  $scope.gocommentdetail=function(notices){
+    var abturl = /\/#\/(\w|\/|#)+/gmi.exec(notices.abturl)[0].split('/#')[1];
+    var urls = /\/#\/(\w|\/|#)+/gmi.exec(notices.abturl)[0].split('/#')[1].split('#');
+    $location.url(urls[0]);
+    $timeout(function(){
+      $location.hash(urls[1])
+    },100)
+
+
+  }
+  $scope.toggle=function(noticeIndex){
+    if(SharedState.get('myNotices')==noticeIndex){
+      SharedState.setOne('myNotices',-1)
+    }else{
+      SharedState.setOne('myNotices',noticeIndex)
+    }
+    if(!$scope.user.msgs[noticeIndex].read){
+      setmsgreaded(noticeIndex)
+    }
+  };
+  function setmsgreaded(index){
+    userservice.setmsgreaded(index)
+    .then(function(result){
+      console.log(result)
+      $scope.user = result.data;
+    })
+  }
+
+  $scope.deleteNotice = function(notice) {
+    var index = $scope.user.msgs.indexOf(notice);
+    if (index > -1) {
+      var n = $scope.user.msgs.splice(index, 1);
+      console.log('deleted notice=',n)
+      commentservice.deleteusercomment(index)
+      .then(function(result){
+        console.log('传回的数据是：',result.data)
+      })
+    }
+  };
+  $scope.testsockit = function(){
+  }
+  socket.on('testobj', function(accountinfo) {
+    console.log('accountinfo是',accountinfo)
+    alert('accountinfo是'+accountinfo)
+  })
+  function genfarray(length){
+    var a =  [true,true,true,true,true,true,true,true];
+    for(var i= 8;i<length;){
+      a = a.concat(a)
+      i=a.length;
+    }
+    return a.slice(0,length);
+  }
+}])
+polls1.controller('UserController', ['userservice','messageService','$scope','SharedState','pollservice','$location','commentservice','socket',function(userservice,messageService,$scope,SharedState,pollservice,$location,commentservice,socket){
+
+  messageService.subscribe('userupdate',function(event,data){
+    $scope.user=data.user;
+  })
+
+  socket.on('newmsg',function(data){
+    $scope.msgrem=true;
+    $scope.msgsnum=data
+   /* hasunread($scope.user.msgs).num+1;*/
+  })
+
+  $scope.islogin=userservice.getUserStatussync().islogin;
+  if(!$scope.islogin){
+    userservice.getUserStatus()
+    .then(function(result){
+      $scope.islogin=result.data.islogin;
+      $scope.account=result.data.account;
+      if($scope.islogin){
+        $scope.user=result.data.user;
+        $scope.user.headpicsrc=$scope.user.img_Url?$scope.user.img_Url:pollservice.getheadpic();
+        $scope.msgrem=hasunread($scope.user.msgs).hasunread;
+        $scope.msgsnum=hasunread($scope.user.msgs).num;
+        $scope.profilebg={
+          'background':'url('+$scope.user.headpicsrc+')',
+           'background-repeat': 'no-repeat',
+           'background-size':'100% 100%'
+        }
+      }
+    })
+  }
+  function hasunread(arr){
+    var hasunread =false,num=0;
+    for(var i=0,len=arr.length;i<len;i++){
+      var item = arr[i];
+      if(!item.read){
+        hasunread=true;
+        num++
+      }
+    }
+    return {hasunread:hasunread,num:num};
+  }
+  $scope.mylatestmsg=function(){
+    $scope.msgrem=false;
+    $location.path('/mymsgs')
+  }
+  $scope.myfavor=function(){
+    $location.path('/myfavor')
+  }
+
+  $scope.updateuser=function(){
+    console.log($scope.user)
+    userservice.updateuser($scope.user)
+    .then(function(result){
+      $scope.user = result.data;
+      $scope.user.headpicsrc=$scope.user.img_Url;
+      SharedState.turnOff('modal15');
+    })
+  }
+  $scope.zommout=function(event){
+    console.log(event)
+    $scope.img_zoom=$scope.img_zoom?false:true;
+  }
+  $scope.getheadpic=function(){
+    $scope.user.headpicsrc=pollservice.getheadpic();
+  };
+
+  $scope.presibling=function($event){
+    var ipt = $event.target.parentNode.nextSibling.nextSibling
+    ipt.click()
+    console.log(ipt)
+  }
+  $scope.login=function(){
+    var account = $scope.user.account;
+    var password = $scope.user.password;
+    $scope.error = false;
+    $scope.disabled = true;
+    userservice.signin({account:account,password:password})
+    .then(function(re){
+      var result =re.data
+      if(result.status=='ok'){
+        $scope.user = result.user;
+        $scope.user.headpicsrc=result.user.img_Url;
+        $scope.islogin = true;
+        userservice.setUserStatussync({islogin:true,account:account})
+        messageService.publish('userlogin',{account:account,islogin:true,user:result.user})
+      }else if(result.status=='wrongpass'){
+        $scope.islogin = false;
+        $scope.msg="Invalid username and/or password. 用户名/密码不对";
+        userservice.setUserStatussync({islogin:false,account:""})
+      }else if(result.status=='notexist'){
+        $scope.islogin = false;
+        $scope.msg='User '+account+' does not exist. 用户'+account+'不存在'
+        userservice.setUserStatussync({islogin:false,account:""})
+      }
+    })
+    .catch(function(){
+      $scope.error = true;
+      $scope.msg = "Invalid username and/or password";
+      $scope.disabled = false;
+      $scope.user = {};
+    });
+  }
+  $scope.logout=function(){
+    userservice.signout()
+    .then(function(re){
+      var result=re.data
+      if(result.status){
+        $scope.islogin = false;
+        $scope.user = {};
+        messageService.publish('userlogin',{account:"",islogin:false})
+      }
+    })
+  }
+  $scope.register=function(){
+    $scope.error = false;
+    $scope.disabled = true;
+    var account = $scope.user.account;
+    var password = $scope.user.password
+    var img_Url = pollservice.getheadpic();
+    userservice.signup({account:account,password:password,img_Url:img_Url})
+    .then(function(re){
+      var result =re.data;
+      if(result.status=='ok'){
+        if($scope.autologin){
+          return userservice.signin({account:account,password:password})
+        }else{
+          return {data:'tologin'}
+        }
+      }else if(result.status =='exist'){
+        console.log("result.status =='exist'")
+        return {data:'exist'}
+      }
+    })
+    .then(function(re){
+      var result =re.data
+      if(result.status=='ok'){
+
+        $scope.user = result.user;
+        $scope.user.headpicsrc=result.user.img_Url;
+        $scope.islogin = true;
+        userservice.setUserStatussync({islogin:true,account:account})
+        messageService.publish('userlogin',{account:account,islogin:true,user:result.user})
+
+        console.log('emituserlogin')
+      }else if(result.status=='wrongpass'){
+        $scope.msg='Password not matched. 密码不对'
+      }else if(result == 'exist'){
+        console.log("result == 'exist'")
+        $scope.msg='User "'+account+'" alrady exist! 用户 '+account+' 已存在';
+      }else if(result=='tologin'){
+        $scope.loginswitch=true;
+      }
+    })
+    .catch(function () {
+      $scope.error = true;
+      $scope.msg = "Something went wrong!";
+      $scope.disabled = false;
+    });
+  }
+}])
